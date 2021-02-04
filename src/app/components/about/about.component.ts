0f17f80be19348e41a294706e4792b4d6680400f
Omar Antonio Garcia Rosado <54970170+OMARANTONIOGARCIA@users.noreply.github.com>
2021-02-04 08:42:10 -0600

    Pipe

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 
 export class AboutComponent implements OnInit {
 
+  nombre:string = 'Capitan América';
+  arreglo: number[] = [1,2,3,4,5,6,7,8,9,10]
+  personajes: string[] =['Iroman','Spiderman', 'Thor', 'Loki', 'Groot'];
+  PI:number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+
   constructor() { }
 
   ngOnInit(): void {
